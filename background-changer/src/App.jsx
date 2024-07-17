b164218df1fbe54bbedebeafefb6bf669434978a
MuhammadShariq05 <shariqwrk12023@gmail.com>
2024-07-17 14:35:45 +0530

    color fixes

--- background-changer/src/App.jsx
+++ background-changer/src/App.jsx
@@ -3,18 +3,20 @@ import './App.css'
 
 function App() {
   const [color, setColor] = useState("white");
-  
+  const handleColorChange = (color) => {
+    setColor(color);
+  };
   return (                 
     <>
        <div className="background" style={{backgroundColor: `${color}`}}>
           <div className='holder'>
-            <button className="red" onClick={()=> setColor("red")}>Red</button>
-            <button className="yellow" onClick={()=> setColor("yellow")}>Yellow</button>
-            <button className="black" onClick={()=> setColor("black")}>Black</button>
-            <button className="purple" onClick={()=> setColor("#535bf2")}>Purple</button>
-            <button className="green" onClick={()=> setColor("green")}>Green</button>
-            <button className="blue" onClick={()=> setColor("blue")}>Blue</button>
-            <button className="white" onClick={()=> setColor("white")}>Default</button>
+            <button className="red" onClick={() => handleColorChange("red")}>Red</button>
+            <button className="yellow" onClick={() => handleColorChange("yellow")}>Yellow</button>
+            <button className="black" onClick={() => handleColorChange("black")}>Black</button>
+            <button className="purple" onClick={() => handleColorChange("#535bf2")}>Purple</button>
+            <button className="green" onClick={() => handleColorChange("green")}>Green</button>
+            <button className="blue" onClick={() => handleColorChange("blue")}>Blue</button>
+            <button className="default" onClick={() => handleColorChange("white")}>Default</button>
           </div>
        </div>
     </>
