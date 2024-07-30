dd6ca4e58c5c3db27fdf8e8e22ed428bd79feb64
MuhammadShariq05 <shariqwrk12023@gmail.com>
2024-07-30 16:51:43 +0530

    declarative function of react

--- use-state/src/App.jsx
+++ use-state/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -12,12 +10,25 @@ function App() {
   // Passing a function instead of value
   // If u give a function to usestate it will run only once
   const [name, setName] = useState(slowSetter)
+  const [age, setAge] = useState(21)
   function handleChange(){
     setName("Shariq")
+    console.log("Before ", age)
+    // In raect decleative nature, the values are not updating unless the whole reacr app is re-rendered.
+    setAge(age + 1)
+    // console.log("After 1", age)
+
+    // setAge(age + 1)
+    // console.log("After 2", age)
+    // When the function is depends on the previous number then use the function instead
+    // setAge((currentAge) => {
+    //   return currentAge + 1
+    // })
+
   }
   return (
     <>
-      <h1 onClick={handleChange}>{ name }</h1>
+      <h1 onClick={handleChange}>Hi, Im { name } <br /> My Age is {age}</h1>
     </>
   )
 }
