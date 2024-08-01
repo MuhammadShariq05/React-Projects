d79d6b44e2b8d43c4ec87d2533b5d9ee79b30c04
MuhammadShariq05 <shariqwrk12023@gmail.com>
2024-08-01 15:16:20 +0530

    fetch api

--- fetch/src/App.jsx
+++ fetch/src/App.jsx
@@ -1,17 +1,42 @@
 import { useState } from "react";
 import "./App.css";
+import { useEffect } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
-  fetch("https://jsonplaceholder.typicode.com/users")
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-    });
+  const [users, setUsers] = useState();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState()
+  useEffect(() => {
+    setLoading(true)
+    setError(undefined)
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(res => {
+        if(res.status === 200) return res.json()
+        else return Promise.reject(res)
+      })
+      .then((data) => {
+        setUsers(data);
+      })
+      .catch(e => setError(e))
+      .finally(() => {
+        setLoading(false);
+      });
+  },[]);
+
+  let jsx;
+  if (loading) {
+    jsx = <h2>Loading...</h2>;
+  } else if(error != null) {
+    jsx = <h2>Error !</h2>
+  } else {
+    jsx = JSON.stringify(users);
+  }
+
   return (
     <>
       <div>
         <h1>User</h1>
+        {jsx}
       </div>
     </>
   );
