e5f74f4dc24eb94ec5264b2b708e2d91af629493
MuhammadShariq05 <shariqwrk12023@gmail.com>
2024-08-01 15:22:17 +0530

    saves

--- fetch/src/App.jsx
+++ fetch/src/App.jsx
@@ -7,33 +7,35 @@ function App() {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState();
 
-  const controller = new AbortController()
+  const controller = new AbortController();
   useEffect(() => {
-    setLoading(true)
-    setError(undefined)
-    fetch("https://jsonplaceholder.typicode.com/users" ,{ signal: controller.signal })
-      .then(res => {
-        if(res.status === 200) return res.json()
-        else return Promise.reject(res)
+    setLoading(true);
+    setError(undefined);
+    fetch("https://jsonplaceholder.typicode.com/users", {
+      signal: controller.signal,
+    })
+      .then((res) => {
+        if (res.status === 200) return res.json();
+        else return Promise.reject(res);
       })
       .then((data) => {
         setUsers(data);
       })
-      .catch(e => setError(e))
+      .catch((e) => setError(e))
       .finally(() => {
         setLoading(false);
       });
 
-      return () => {
-        controller.abort()
-      }
-  },[]);
+    return () => {
+      controller.abort();
+    };
+  }, []);
 
   let jsx;
   if (loading) {
     jsx = <h2>Loading...</h2>;
-  } else if(error != null) {
-    jsx = <h2>Error !</h2>
+  } else if (error != null) {
+    jsx = <h2>Error !</h2>;
   } else {
     jsx = JSON.stringify(users);
   }
