b926cad9a88a34f9b97013d714348817b7f62f96
MuhammadShariq05 <shariqwrk12023@gmail.com>
2024-08-01 15:40:03 +0530

    Initial commit

---
+++ react-dev-tools/src/FuncComp.jsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import { DisplayString } from "./DisplayString"
+
+export function FuncComp() {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+
+  useEffect(() => {
+    document.title = name
+  }, [name])
+
+  return (
+    <>
+      <div>
+        <input
+          type="text"
+          placeholder={"Enter your name here..."}
+          value={name}
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        />
+        <h3 className="profile">
+          <DisplayString name={name} age={age}/>
+        </h3>
+        <h2>{age}</h2>
+        <button className="counter" onClick={() => setAge(currentAge => currentAge - 1)}>
+          -
+        </button>
+        <button className="counter" onClick={() => setAge(currentAge => currentAge + 1)}>
+          +
+        </button>
+        <br />
+        <br />
+      </div>
+    </>
+  );
+}
