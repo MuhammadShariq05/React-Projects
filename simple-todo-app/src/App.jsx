69afd42620360e10434a04f3a6675f0a89fbc98a
MuhammadShariq05 <shariqwrk12023@gmail.com>
2024-08-02 21:34:16 +0530

    completed

--- simple-todo-app/src/App.jsx
+++ simple-todo-app/src/App.jsx
@@ -1,20 +1,68 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [newTodo, setNewTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  function addTodos() {
+    if (newTodo === "") return alert("Empty Input");
+    setTodos((currentTodos) => {
+      return [
+        ...currentTodos,
+        { name: newTodo, completed: false, id: crypto.randomUUID() },
+      ];
+    });
+    setNewTodo("");
+  }
+
+  function toggleTodo(todoId, completed){
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if(todo.id === todoId) return {...todo, completed}
+        else return todo
+      })
+    })
+  }
 
   return (
     <>
       <div className="new-todo-form">
-        <label htmlFor="todo-input"><h1>Todos</h1></label>
+        <label htmlFor="todo-input">
+          <h1>Todos</h1>
+        </label>
         <br />
-        <input type="text" id="todo-input" placeholder='Write Todo here'/>
-        <button>Add Todo</button>
+        <input
+          type="text"
+          id="todo-input"
+          placeholder="Write Todo here"
+          value={newTodo}
+          onChange={(e) => {
+            setNewTodo(e.target.value);
+          }}
+        />
+        <button onClick={addTodos}>Add Todo</button>
+        <ul id="list">
+          {todos.map((todo) => {
+            return (
+              <li key={todo.id} className="list-item">
+                <label className="list-item-label">
+                  <input
+                    checked={todo.completed}
+                    type="checkbox"
+                    data-list-item-checkbox
+                    onChange={(e) => toggleTodo(todo.id, e.target.checked)}
+                  />
+                  <span className="span-items"data-list-item-text>{todo.name}</span>
+                </label>
+                <button className="delete-items"data-button-delete>Delete</button>
+              </li>
+            );
+          })}
+        </ul>
       </div>
     </>
-  )
+  );
 }
 
-export default App
- 
+export default App;
